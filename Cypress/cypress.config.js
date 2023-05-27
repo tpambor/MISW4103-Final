cfc2246df2e27edc1a1095ce1c948719b2c8bd70
Tim Pambor <t.pambor@uniandes.edu.co>
2023-05-28 00:17:38 +0200

    Enable screenshots and firefox

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -23,6 +23,11 @@ module.exports = defineConfig({
           launchOptions.args.push('--window-size=1920,1080');
         }
 
+        if (browser.name === 'firefox' && browser.isHeadless) {
+          launchOptions.args.push('--width=1920');
+          launchOptions.args.push('--height=1080');
+        }
+
         return launchOptions;
       })
     },
