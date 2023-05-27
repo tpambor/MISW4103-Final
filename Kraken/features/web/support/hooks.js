ff97787c7d7b0558c1bcfa93e319e39c5427d3f9
svhurtado <sandra.victoria.hurtado@gmail.com>
2023-05-26 19:50:36 -0500

    Add support for screenshots

--- Kraken/features/web/support/hooks.js
+++ Kraken/features/web/support/hooks.js
@@ -1,9 +1,26 @@
-const { After, Before } = require('@cucumber/cucumber');
+const { After, Before, AfterStep } = require('@cucumber/cucumber');
 const { WebClient } = require('kraken-node');
+const properties = require('../../../properties.json');
+const takeshots = properties.Screenshots;
+const Screenshots = require('./screenshots');
+let screenshots;
 
 Before(async function(scenario) {
   this.deviceClient = new WebClient('chrome', {}, this.userId);
   this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
+
+  if (takeshots == "YES") {
+    const path = scenario.pickle.name.replace(/\s+/g, '');
+    screenshots = new Screenshots(path);
+    screenshots.createScreenshotsDir();
+    screenshots.createFunctionDir();
+  }
+});
+
+AfterStep(async function() {
+  if (takeshots == "YES") {
+    await this.driver.saveScreenshot(screenshots.getName());
+  }
 });
 
 After(async function() {
